6b5e620f5265d9ea8f6e6b6f569b4321f5d801f7
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2023-01-04 11:27:49 +0600

    Update Shop.sol

--- Shop.sol
+++ Shop.sol
@@ -1,8 +1,4 @@
-// SPDX-License-Identifier: MIT
-// Create Token Here - https://wizard.openzeppelin.com/
-// Remix IDE - https://remix.ethereum.org/
-// Ethereum Converter - https://eth-converter.com/
-
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.15;
 
@@ -12,49 +8,77 @@ import "./Token.sol";
 
 contract ICOShop is Ownable { 
 
-    // here we created a 
     CreativeCoin public token; 
     uint public rate; 
 
-    constructor (uint tokenAmount, uint rateAmount){
-        changeOwner(msg.sender);
+    constructor ( uint tokenAmount , uint rateAmount ) { 
         createTokenForSell(tokenAmount);
-        rate = rateAmount;
-        
+        changeOwnerToken(msg.sender);
+        rate = rateAmount; 
+
     }
 
-    function createTokenForSell(uint _valueTokenForSell ) internal onlyOwner {
-        CreativeCoin newCC  = new CreativeCoin(_valueTokenForSell);
-        token = CreativeCoin(address(newCC));
+    // function that will deploy our token
+    function createTokenForSell ( uint _valueTokenForSale ) internal onlyOwner {
+        CreativeCoin newCreativeCoin = new CreativeCoin(_valueTokenForSale);
+        token = CreativeCoin(address(newCreativeCoin));
     }
 
-    // change the Owner function
-    function changeOwner( address _newOwner) internal onlyOwner {
-        // the tranferOwnership function comes from the Ownable library
-        token.transferOwnership(_newOwner);
+    //change Owner Function 
+    function changeOwnerToken ( address _newOwner ) internal onlyOwner { 
+        token.transferOwnership(_newOwner );
     }
 
-    // new rate function
-    function newrate (uint _newRate) external onlyOwner{
-        require(_newRate > 0, " Cannot have a rate that is less than or equal to 0");
-        rate = _newRate;
-    }
 
-    // new address token function
-    function newToken (address _adr ) external onlyOwner {
-        require( _adr != address(0), "zero address !!");
+    // get token balance on this contract 
+    function tokenBalance () public  view returns ( uint ) { 
+        return token.balanceOf(address(this));
+    }
+    // get this contract balance eher 
+    function etherBalance () public view returns ( uint ) { 
+        return address(this).balance;
+    }
+    // new address token function 
+    function newToken ( address _adr ) external  onlyOwner { 
+        require(_adr != address(0) , "Zerro Address!");
         token = CreativeCoin(_adr);
     }
-
-    // funciton to withdraw all token if they are not selling
-    function tokenWithdraw() external onlyOwner{
-        require(tokenBalance() > 0, "TokenBalance is 0");
-        token.transfer( owner(), tokenBalance());
+    // new Rate function
+    function newRate ( uint amount )  external onlyOwner { 
+        require(amount > 0 , "Rejected with 0 amount ");
+        rate = amount;
     }
-
-    // function to withdraw ethereum from the contract
-    function tokenBalance() public view returns(uint){
-       return token.balanceOf(address(this));
+    //func withdraw all tokens if they are not selling 
+    function tokenWithdraw () external onlyOwner { 
+        require( tokenBalance() > 0 , " 0 amount of tokenBalance ");
+        token.transfer(owner(), tokenBalance());
+    }
+    //func withdraw etherum from contract 
+    function etherWithdrwaw () external onlyOwner { 
+        require( address(this).balance > 0 , " 0 amount of contract balance ");
+        payable(owner()).transfer(address(this).balance);
+    }
+    //buyToken Funciton 
+    function buyToken () external payable {
+        require(msg.value > 0 , " Rejected ");
+        uint tokensAmount = msg.value / rate; 
+        require( tokensAmount <= tokenBalance (), " Rejected " );
+        token.transfer(msg.sender, tokensAmount);
+    }
+    //sellToken Function
+    function sellToken ( uint _tokenAmount ) external { 
+        uint allowanceToken = token.allowance(msg.sender, address(this));
+        uint etheramount = _tokenAmount * rate; 
+        require( allowanceToken >= _tokenAmount, "rejected "); 
+        token.transferFrom(msg.sender , address(this), _tokenAmount);
+        payable(msg.sender).transfer(etheramount);
     }
 
 }
+
+// Create Token Here - https://wizard.openzeppelin.com/
+// Remix IDE - https://remix.ethereum.org/
+// Ethereum Converter - https://eth-converter.com/
+
+
+
